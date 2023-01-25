116e8a8e7803357096a31f7ce94990c2bd4f5bf9
Luisa Mazzi <luisa.c.mazzi@gmail.com>
2023-01-25 15:45:46 -0300

    Initial project updated and locationpermission added to manifest

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +8,25 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  Future<void> getLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+
+    // await Geolocator.openAppSettings();
+    // await Geolocator.openLocationSettings();
+
+    //Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
+        child: ElevatedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
